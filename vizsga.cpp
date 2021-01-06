76cb4a48fd202046c7af97b02db7adeeb72686f2
poloczmate <48856366+poloczmate@users.noreply.github.com>
2021-01-06 14:53:38 +0100

    help needded

--- vizsga.cpp
+++ vizsga.cpp
@@ -1,20 +1,99 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
+class Lamp{
+private:
+    bool on = false;
+    string name;
+public:
+    Lamp(string name) : name(name) {}
+    bool isOn(){
+        return on;
+    }
+    void setOn(){
+        on = true;
+    }
+    void setOff(){
+        on = false;
+    }
+    string getName(){
+        return name;
+    }
+    void flip(){
+        if (isOn())
+        {
+            on = false;
+            cout << getName() << "turned OFF" << std::endl;
+        }else{
+            on = true;
+            cout << getName() << "turned ON" << std::endl;
+        }
+        
+    }
+};
 
+class DimmableLamp : public Lamp{
+    int fenyero = 100;
+public:
+    DimmableLamp(string name) : Lamp(name) {}
+    void setBrightness(int a) {
+        fenyero += a;
+        if(fenyero > 100) fenyero = 100;
+        if(fenyero < 1) fenyero = 1;
+    }
+    int getBrightness(){
+        if (!isOn())
+        {
+            return 0;
+        }
+        return fenyero;
+    }
+    void flip override(){
+        if (!isOn())
+        {   
+            setOn();
+            cout << getName() << " turned ON at " << getBrightness() << "brightness" << std::endl;
+        }else{
+            setOff();
+            cout << getName() << " turned OFF" << std::endl;
+            
+        }
+    }
 
-
+};
 //innentol main **************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************
-int main(){
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+int main()
+{
+    using namespace std;
+
+    Lamp *l1 = new Lamp("Desk lamp");
+    cout << "Test 1: " << l1->isOn() << endl;
+    l1->flip();
+    cout << "Test 2: " << l1->isOn() << endl;
+    l1->flip();
+    cout << "Test 3: " << l1->isOn() << endl;
+
+    cout << "DimmableLamp tests" << endl;
+    DimmableLamp *dl = new DimmableLamp("Ceiling lamp");
+    Lamp *l2 = dl;
+    cout << "Test 4: " << l2->isOn() << endl;
+    cout << "Test 5: " << dl->getBrightness() << endl;
+    l2->flip();
+    cout << "Test 6: " << l2->isOn() << endl;
+    cout << "Test 7: " << dl->getBrightness() << endl;
+    dl->setBrightness(50);
+    cout << "Test 8: " << dl->getBrightness() << endl;
+    l2->flip();
+    cout << "Test 9: " << l2->isOn() << endl;
+    l2->flip();
+    dl->setBrightness(0);
+    cout << "Test 10: " << dl->getBrightness() << endl;
+    dl->setBrightness(101);
+    cout << "Test 11: " << dl->getBrightness() << endl;
+
+    delete l1;
+    delete l2;
+
     return 0;
 }
